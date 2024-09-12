f36496d0cbbc82f1c5624af0726a08bb47784a9f
israelcruzz <israelcruz.contato@gmail.com>
2024-09-12 11:59:51 -0300

    feat: add structure html in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <section>
+        <header>
+          <nav>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Contacts</a>
+          </nav>
+        </header>
+
+        <h1>Protecting Nature for a Sustainable future</h1>
+
+        <span>
+          Look throught nature to understand what is nature realy are and feel
+          this way to make sure you do right things to save that beautiful
+          world.
+        </span>
+
+        <button>See more</button>
+      </section>
+
+      <section>
+        <div>
+          <h1>Hi Guys!!</h1>
+          <span>
+            Welcome to a world where nature is valued, protected and inspires us
+            all to care for it. We, the freeway team, are passionate defenders
+            of our planet, dedicated to not only preserving its unique beauty
+            and diversity, but also inspiring others to participate in this
+            important cause.{" "}
+          </span>
+        </div>
+
+        <div>
+          <img src="" alt="" />
+        </div>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
